66b39679d59f0fba6f7376370c58f1c76cb7abec
TravisSLee <travisslee223@gmail.com>
2021-03-31 11:33:29 -0400

    commit tree 6b627993fd8670a9f7a1998514accfde7e85ffc3

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const moviesList = movies.map(movie => {
+    return (
+      <div>
+        <ul>
+          <h2>{movie.title}</h2>
+          <p>{movie.time}</p>
+          <p>{movie.genres.join(' ,')}</p>
+        </ul>
+      </div>
+    )
+  })
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {moviesList}
     </div>
   );
 };
